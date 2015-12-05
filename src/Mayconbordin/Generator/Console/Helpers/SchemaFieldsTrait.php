5487baca4346dcc9fa43ef7e67ce89940b28eb09
Maycon Bordin <mayconbordin@gmail.com>
2015-12-04 22:51:51 -0200

    Created trait to retrieve table(s) from command line arguments and database connection

---
+++ src/Mayconbordin/Generator/Console/Helpers/SchemaFieldsTrait.php
@@ -0,0 +1,82 @@
+<?php namespace Mayconbordin\Generator\Console\Helpers;
+
+use Mayconbordin\Generator\Database\SchemaGenerator;
+use Mayconbordin\Generator\Exceptions\MethodNotFoundException;
+use Mayconbordin\Generator\Generator\MigrationGenerator;
+use Mayconbordin\Generator\Parsers\NameParser;
+use Mayconbordin\Generator\Parsers\SchemaParser;
+use Mayconbordin\Generator\Schema\Table;
+
+trait MigrationTrait
+{
+    /**
+     * @var SchemaGenerator
+     */
+    protected $schemaGenerator;
+
+    /**
+     * Get a table object from the command line arguments (name and fields).
+     *
+     * @return Table
+     */
+    protected function parseTableFromCli()
+    {
+        $meta   = (new NameParser())->parse($this->argument('name'));
+        $fields = (new SchemaParser())->parse($this->option('fields'));
+
+        return new Table($meta['table'], $fields);
+    }
+
+    /**
+     * Get a list of table objects from the database connection.
+     *
+     * @throws MethodNotFoundException
+     */
+    protected function parseSchemaFromDb()
+    {
+        $this->schemaGenerator = new SchemaGenerator(
+            $this->option('connection'),
+            $this->option('defaultIndexNames'),
+            $this->option('defaultFKNames')
+        );
+
+        if ($this->option('tables')) {
+            $tables = explode(',', $this->option('tables'));
+        } else {
+            $tables = $this->schemaGenerator->getTables();
+        }
+
+        $tables = $this->removeExcludedTables($tables);
+        return $this->schemaGenerator->getSchema($tables);
+    }
+
+    /**
+     * Remove all the tables to exclude from the array of tables
+     *
+     * @param $tables
+     *
+     * @return array
+     */
+    protected function removeExcludedTables($tables)
+    {
+        $excludes = $this->getExcludedTables();
+        $tables = array_diff($tables, $excludes);
+        return $tables;
+    }
+    /**
+     * Get a list of tables to exclude
+     *
+     * @return array
+     */
+    protected function getExcludedTables()
+    {
+        $excludes = ['migrations'];
+        $ignore = $this->option('ignore');
+
+        if (!empty($ignore)) {
+            return array_merge($excludes, explode(',', $ignore));
+        }
+
+        return $excludes;
+    }
+}
